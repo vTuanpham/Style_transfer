26851e8ad5981cbcd1a2fdf50308aafc32fd8c88
Tuan-Pham <weekend.2810@gmail.com>
2023-06-14 12:10:00 +0700

    chore: update training script

--- src/scripts/train.sh
+++ src/scripts/train.sh
@@ -2,7 +2,7 @@ python src/train.py --output_dir "./src/models/checkpoints/training_session"    
                    --content_datapath "./src/data/dummy/content"  \
                    --style_datapath "./src/data/dummy/style"      \
                    --batch_size 5                                \
-                   --max_style_train_samples 20                  \
+                   --max_style_train_samples 1000                \
                    --max_content_train_samples 1000              \
                    --max_eval_samples 5                          \
                    --seed 42                                     \
@@ -20,6 +20,8 @@ python src/train.py --output_dir "./src/models/checkpoints/training_session"    
                    --style_layers_idx 1 3 6 8 11                 \
                    --transformer_size 32                         \
                    --CNN_layer_depth 4                           \
-                   --deep_learner
+                   --deep_learner                                \
+                   --with_tracking                               \
+                   --log_weights_cpkt
 
 
