2044c69ba5e05e84038ffcf0ed9725d9e4c28ea1
Tuan-Pham <weekend.2810@gmail.com>
2023-06-18 17:27:50 +0700

    feat: add custom aug for data, add mutiple path support

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,6 +1,7 @@
 import os
 import random
 import warnings
+from typing import List
 
 import PIL.Image
 import torch
@@ -12,10 +13,12 @@ from torch.utils.data import DataLoader, Dataset, RandomSampler
 
 
 class STDataset(Dataset):
-    def __init__(self, content_urls: str, style_urls: str, transform=None):
+    def __init__(self, content_urls: str, style_urls: str,
+                 transform_content=None, transform_style=None):
         self.content_urls = content_urls
         self.style_urls = style_urls
-        self.transform = transform
+        self.transform_content = transform_content
+        self.transform_style = transform_style
 
     def __len__(self):
         return len(self.content_urls) if len(self.content_urls) < len(self.style_urls) else len(self.style_urls)
@@ -31,9 +34,10 @@ class STDataset(Dataset):
             warnings.warn("IO Error, watch out !")
             raise "IO Error!"
 
-        if self.transform is not None:
-            content_image = self.transform(content_image)
-            style_image = self.transform(style_image)
+        if self.transform_content is not None:
+            content_image = self.transform_content(content_image)
+        if self.transform_style is not None:
+            style_image = self.transform_style(style_image)
 
         return {'content_image': content_image,
                 'style_image': style_image}
@@ -41,16 +45,18 @@ class STDataset(Dataset):
 
 class STDataloader:
     def __init__(self,
-                 content_datapath: str,
-                 style_datapath: str,
+                 content_datapath: List[str],
+                 style_datapath: List[str],
                  batch_size: int,
-                 transform,
+                 transform_content,
+                 transform_style,
                  max_style_train_samples: int,
                  max_content_train_samples: int,
                  max_eval_samples: int,
                  seed: int):
         self.batch_size = batch_size
-        self.transform = transform
+        self.transform_content = transform_content
+        self.transform_style = transform_style
         self.seed = seed
         self.max_style_train_samples = max_style_train_samples
         self.max_content_train_samples = max_content_train_samples
@@ -63,30 +69,33 @@ class STDataloader:
 
     def __call__(self, *args, **kwargs):
         # Only loading the image url to save on Ram
-
-        content_datapath_list = os.listdir(self.content_datapath)[:self.max_content_train_samples]
         content_image_urls = []
-        for url in content_datapath_list:
-            if os.path.isfile(os.path.join(self.content_datapath, url)):
-                extension = url.split(".")[-1]
-                assert extension in ["jpg"] or extension in ["png"], "non-image file found in dir."
-                content_image_urls.append(os.path.join(self.content_datapath, url))
-            else:
-                warnings.warn("Non file found in data dir")
-                continue
+        for path in self.content_datapath:
+            content_datapath_list = os.listdir(path)
+            for url in content_datapath_list:
+                if os.path.isfile(os.path.join(path, url)):
+                    extension = url.split(".")[-1]
+                    assert extension in ["jpg"] or extension in ["png"], "non-image file found in dir."
+                    content_image_urls.append(os.path.join(path, url))
+                else:
+                    warnings.warn("Non file found in data dir")
+                    continue
 
-        style_datapath_list = os.listdir(self.style_datapath)[:self.max_style_train_samples]
         style_image_urls = []
-        for url in style_datapath_list:
-            if os.path.isfile(os.path.join(self.style_datapath, url)):
-                extension = url.split(".")[-1]
-                assert extension in ["jpg"] or extension in ["png"], "non-image file found in dir."
-                style_image_urls.append(os.path.join(self.style_datapath, url))
-            else:
-                warnings.warn("Non file found in data dir")
-                continue
+        for path in self.style_datapath:
+            style_datapath_list = os.listdir(path)
+            for url in style_datapath_list:
+                if os.path.isfile(os.path.join(path, url)):
+                    extension = url.split(".")[-1]
+                    assert extension in ["jpg"] or extension in ["png"], "non-image file found in dir."
+                    style_image_urls.append(os.path.join(path, url))
+                else:
+                    warnings.warn("Non file found in data dir")
+                    continue
 
-        dataset = STDataset(content_image_urls, style_image_urls, transform=self.transform)
+        dataset = STDataset(content_image_urls[:self.max_content_train_samples],
+                            style_image_urls[:self.max_style_train_samples],
+                            transform_content=self.transform_content, transform_style=self.transform_style)
         return self.get_dataloader(dataset, shuffle_flag=True)
 
     def get_dataloader(self, dataset, shuffle_flag: bool = False):
